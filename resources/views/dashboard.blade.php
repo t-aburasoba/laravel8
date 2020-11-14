a3f2e2753ec19687dc90969020162b7fe46f5c29
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2020-11-14 20:29:20 +0900

    modify top little

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="font-semibold text-3xl text-gray-800 leading-tight">Toileatter</h1>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-5">
+        <h1 class="font-semibold text-3xl text-gray-800 leading-tight">
             {{ __('あなたの投稿が腹痛民を救います。') }}
+        </h1>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-5">
+            {{ __('Toileatter（トイレアッター）は駅構内のトイレを評価しシェアするアプリです。') }}
         </h2>
 
     </x-slot>
